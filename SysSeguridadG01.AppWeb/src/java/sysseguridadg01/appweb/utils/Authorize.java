69a63bd9c5762392fbecae542cc9c08e06bd8641
Wiliam <lwillian197@gmail.com>
2023-07-31 10:30:48 -0600

    commit

---
+++ SysSeguridadG01.AppWeb/src/java/sysseguridadg01/appweb/utils/Authorize.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package sysseguridadg01.appweb.utils;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+
+public interface Authorize {
+    void authorize () throws ServletException, IOException;
+}
